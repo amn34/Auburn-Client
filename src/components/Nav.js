8adfb038e222186963c145f6d8af63c0647c2e5a
dlbjames <dljames@uw.edu>
2021-04-10 22:41:29 -0700

    Added searching functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,14 @@
 import { Navbar, Form, Button, FormControl } from 'react-bootstrap'
 import { FaSearch } from 'react-icons/fa'
 
-const Nav = () => {
+const Nav = ({location="AL", setLocation}) => {
     return (
         <Navbar expand="lg">
             <Navbar.Brand>COVID Stats</Navbar.Brand>
             <FaSearch />
-            <Form inline style={navStyle}>
-                <FormControl type="text" placeholder="Type a city/state" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
+            <Form onSubmit={function(e) {parseLocation(location); e.preventDefault();}} inline style={navStyle}>
+                <FormControl onSubmit={() => parseLocation(location)} onChange={(e) => location = e.target.value} id="location" type="text" placeholder="Type a city/state" className="mr-sm-2" />
+                <Button onClick={() => parseLocation(location)} variant="outline-success">Search</Button>
             </Form>
         </Navbar>
     )
@@ -16,6 +16,24 @@ const Nav = () => {
 
 const navStyle = {
     padding: '0 10px'
+}
+
+async function parseLocation(location) {
+    // String split by city and state
+    const potentialStateCity = location.split(", ")
+    const covidStatsApi = 'https://fabled-decker-310400.wl.r.appspot.com/'
+
+    // // state // state/city
+    const potentialLocation = potentialStateCity.length > 1 ? `${potentialStateCity[1]}/${potentialStateCity[0]}` : `${potentialStateCity[0]}`
+    const requestDataForLocation = await fetch(covidStatsApi+potentialLocation).catch(function() {
+        console.log("One of the locations seems to be invalid")
+    })
+
+    const locationData = await requestDataForLocation.json()
+
+    console.log(locationData)
+
+    return locationData
 }
 
 export default Nav
